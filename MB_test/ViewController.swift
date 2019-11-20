a4e870732919bb9dad45916e6534e2bce6773471
Aydar <BigDude6@yandex.ru>
2019-11-20 19:56:34 +0500

    Initial Commit

---
+++ MB_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MB_test
+//
+//  Created by Zayd on 20.11.2019.
+//  Copyright © 2019 Zayd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
