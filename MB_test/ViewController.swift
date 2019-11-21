596ce2954520689da51469fd26036343d1e16088
Aydar <BigDude6@yandex.ru>
2019-11-21 21:14:46 +0500

    Creating project

--- MB_test/ViewController.swift
+++ MB_test/ViewController.swift
@@ -7,9 +7,25 @@
 //
 
 import UIKit
-
+import SwiftyJSON
 class ViewController: UIViewController {
 
+    @IBAction func getData(_ sender: UIButton) {
+        let baseURL = URL(string: "https://rickandmortyapi.com/api/character/")!
+        let task = URLSession.shared.dataTask(with: baseURL) {(data, response, error) in
+            guard let data = data else { return }
+            
+            if let json = try? JSON(data: data){
+                print(json)
+            }
+            
+            //print(String(data: data, encoding: .utf8)!)
+        }
+        
+        task.resume()
+        //let json = JSON(data: datajs!)
+    }
+    @IBOutlet weak var contentScrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
